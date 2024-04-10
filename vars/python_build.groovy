8de2407028a59fb1a717f953980118b60379e23f
zeamort <zeamort@gmail.com>
2024-04-09 19:51:39 -0700

    updated so that each stage commands are done in the service subdirectory using dir

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -6,44 +6,54 @@ def call(dockerRepoName, imageName, portNum, service) {
         }
         stages {
             stage('Build') {
-                steps {
-                    sh "cd ${service}"
-                    sh 'if [ -d ".venv" ]; then rm -Rf .venv; fi'
-                    sh 'python3 -m venv .venv'
-                    sh '. ./.venv/bin/activate'
-                    sh 'pip install -r requirements.txt --break-system-packages'
-                    sh 'pip install --upgrade flask --break-system-packages'
+                dir(service) {
+                    steps {
+                        sh "cd ${service}"
+                        sh 'if [ -d ".venv" ]; then rm -Rf .venv; fi'
+                        sh 'python3 -m venv .venv'
+                        sh '. ./.venv/bin/activate'
+                        sh 'pip install -r requirements.txt --break-system-packages'
+                        sh 'pip install --upgrade flask --break-system-packages'
+                    }
                 }
             }
             stage('Python Lint') {
-                steps {
-                    sh 'pylint --fail-under 5 *.py'
+                dir(service) {
+                    steps {
+                        sh 'pylint --fail-under 5 *.py'
+                    }
                 }
             }
             stage('Security Check'){
-                steps {
-                    sh 'echo hello'
+                dir(service) {
+                    steps {
+                        sh 'echo hello'
+                    }
                 }
             }
             stage('Package') {
-                when {
-                    expression { env.GIT_BRANCH == 'origin/main' }
-                }
-                steps {
-                    withCredentials([string(credentialsId: 'DockerHubMorteza', variable: 'TOKEN')]) {
-                        sh "docker login -u 'zeamort' -p '$TOKEN' docker.io"
-                        sh "docker build -t ${dockerRepoName}:latest --tag zeamort/${dockerRepoName}:${imageName} ."
-                        sh "docker push zeamort/${dockerRepoName}:${imageName}"
+                dir(service) {
+                    when {
+                        expression { env.GIT_BRANCH == 'origin/main' }
+                    }
+                    steps {
+                        withCredentials([string(credentialsId: 'DockerHubMorteza', variable: 'TOKEN')]) {
+                            sh "docker login -u 'zeamort' -p '$TOKEN' docker.io"
+                            sh "docker build -t ${dockerRepoName}:latest --tag zeamort/${dockerRepoName}:${imageName} ."
+                            sh "docker push zeamort/${dockerRepoName}:${imageName}"
+                        }
                     }
                 }
             }
             stage('Deliver') {
-                when {
-                    expression { params.DEPLOY }
-                }
-                steps {
-                    sh "docker stop ${dockerRepoName} || true && docker rm ${dockerRepoName} || true"
-                    sh "docker run -d -p ${portNum}:${portNum} --name ${dockerRepoName} ${dockerRepoName}:latest"
+                dir(service) {
+                    when {
+                        expression { params.DEPLOY }
+                    }
+                    steps {
+                        sh "docker stop ${dockerRepoName} || true && docker rm ${dockerRepoName} || true"
+                        sh "docker run -d -p ${portNum}:${portNum} --name ${dockerRepoName} ${dockerRepoName}:latest"
+                    }
                 }
             }
         }
